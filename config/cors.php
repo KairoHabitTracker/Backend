61dbcc624ffabca9303f32fd8b584f306f33e4b2
Oliver Gruba <olivergruba7@gmail.com>
2026-04-03 14:17:08 +0200

    allowed origins *, patterns removed

--- config/cors.php
+++ config/cors.php
@@ -5,24 +5,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost',
-        'http://127.0.0.1',
-        'http://localhost:19006',
-        'http://127.0.0.1:19006',
-        'http://localhost:8081',
-        'http://127.0.0.1:8081',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-    ],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [
-        '/^http:\/\/(localhost|127\\.0\\.0\\.1)(:\\d+)?$/',
-        '/^http:\\/\\/192\\.168\\.\\d{1,3}\\.\\d{1,3}(:\\d+)?$/', // LAN dev
-        '/^https:\\/\\/[a-z0-9-]+\\.ngrok-free\\.app$/',          // ngrok
-    ],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
